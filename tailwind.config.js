cef5cbda8fa33ecce7c152cdafba1e7b74dd0aca
ShotMeow <shotmeow@gmail.com>
2024-01-16 14:07:09 +0300

    Init: TailwindCSS + SASS, Prettier support, Lint-staged (pre-commit)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-dark": "#1E1E1E",
+      },
+      fontFamily: {
+        sans: ["var(--font-montserrat)"],
+      },
+    },
+  },
+  plugins: [],
+};
