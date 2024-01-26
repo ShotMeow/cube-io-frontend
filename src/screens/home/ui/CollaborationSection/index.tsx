80e894c8cbc7d993c978cf635cc881fe08169873
ShotMeow <shotmeow@gmail.com>
2024-01-26 16:07:35 +0300

    Added: Collaborations Section in Main Page

---
+++ src/screens/home/ui/CollaborationSection/index.tsx
@@ -0,0 +1,33 @@
+import type { FC } from "react";
+
+import styles from "./CollaborationSection.module.scss";
+import { collaborationsData } from "../../data/collaborations.data";
+
+import Star from "@/shared/ui/Star";
+import Planet from "@/shared/ui/Planet";
+
+const CollaborationSection: FC = () => {
+  return (
+    <section className={styles.section}>
+      <h2>Collaboration</h2>
+      <h3>
+        Unlimited Possibility, <br /> Unlimited Collaboration
+      </h3>
+      <ul>
+        {collaborationsData.map((collaboration) => (
+          <li key={collaboration}>
+            <span>
+              <Star />
+            </span>
+            {collaboration}
+          </li>
+        ))}
+      </ul>
+      <span className={styles.planet}>
+        <Planet />
+      </span>
+    </section>
+  );
+};
+
+export default CollaborationSection;
